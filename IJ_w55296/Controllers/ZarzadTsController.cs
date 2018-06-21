56d96e9a78a617f3c56d02ef2ce329d7f7d21a21
biologiawbinoz <biologiawbinoz@gmail.com>
2018-06-22 00:01:58 +0200

    Bazowa aplikacja

--- IJ_w55296/Controllers/ZarzadTsController.cs
+++ IJ_w55296/Controllers/ZarzadTsController.cs
@@ -17,51 +17,10 @@ namespace IJ_w55296.Controllers
     {
         private DatabazeFirst_3Entities db = new DatabazeFirst_3Entities();
 
-        // GET: ZarzadTs
-       public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        // GET: OrganizacjeTs
+        public ActionResult Index()
         {
-            ViewBag.CurrentSort = sortOrder;
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            ViewBag.OrSortParm = sortOrder == "Organizations" ? "or_desc" : "Organizations";
-
-            if (searchString != null)
-            {
-                page = 1;
-            }
-            else
-            {
-                searchString = currentFilter;
-            }
-
-            ViewBag.CurrentFilter = searchString;
-
-            var menagement = from s in db.ZarzadT
-                           select s;
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                menagement = menagement.Where(s => s.Nazwisko.Contains(searchString)
-                                        || s.Imie.Contains(searchString));
-                                        
-            }
-            switch (sortOrder)
-            {
-                case "name_desc":
-                    menagement = menagement.OrderByDescending(s => s.Nazwisko);
-                    break;
-                case "Organizations":
-                    menagement = menagement.OrderBy(s => s.OrganizacjeT.Nazwa);
-                    break;
-                case "or_desc":
-                    menagement = menagement.OrderByDescending(s => s.OrganizacjeT.Nazwa);
-                    break;
-                default:  // Name ascending 
-                    menagement = menagement.OrderBy(s => s.Nazwisko);
-                    break;
-            }
-
-            int pageSize = 2;
-            int pageNumber = (page ?? 1);
-            return View(menagement.ToPagedList(pageNumber, pageSize));
+            return View(db.ZarzadT.ToList());
         }
 
 
